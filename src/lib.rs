273ce011aa01169f9c276b42a6273cdfd6ae6f4f
gjf2a <ferrer@hendrix.edu>
2022-11-18 15:52:49 -0600

    Updated to Rust 2021. Added a couple of methods.

--- src/lib.rs
+++ src/lib.rs
@@ -9,84 +9,85 @@ pub struct Kmeans<T, V: Copy + PartialEq + PartialOrd, D: Fn(&T,&T) -> V> {
 
 impl <T: Clone + PartialEq, V: Copy + PartialEq + PartialOrd + Into<f64>, D: Fn(&T,&T) -> V> Kmeans<T,V,D> {
     pub fn new<M: Fn(&Vec<&T>) -> T>(k: usize, data: &[T], distance: D, mean: M) -> Kmeans<T,V,D> {
-        Kmeans {means: kmeans_iterate(k, data, &distance, &mean), distance}
+        Kmeans {means: Self::kmeans_iterate(k, data, &distance, &mean), distance}
     }
 
     #[cfg(test)]
     pub fn k(&self) -> usize {self.means.len()}
 
-    #[cfg(test)]
     pub fn classification(&self, sample: &T) -> usize {
-        classify(sample, &self.means, &self.distance)
+        Self::classify(sample, &self.means, &self.distance)
+    }
+
+    pub fn mean(&self, classification: usize) -> T {
+        self.means[classification].clone()
     }
 
     #[cfg(test)]
     pub fn copy_means(&self) -> Vec<T> {self.means.clone()}
 
     pub fn move_means(self) -> Vec<T> {self.means}
-}
 
-pub fn initial_plus_plus<T: Clone + PartialEq, V: Copy + PartialEq + PartialOrd + Into<f64>, D: Fn(&T,&T) -> V>
-(k: usize, distance: &D, data: &[T]) -> Vec<T> {
-    let mut result = Vec::new();
-    let mut candidates: Vec<T> = data.iter().map(|t| t.clone()).collect();
-    let range = Uniform::new(0, candidates.len());
-    let mut rng = thread_rng();
-    while candidates.len() < k {
-        candidates.push(data[range.sample(&mut rng)].clone());
+    pub fn initial_plus_plus(k: usize, distance: &D, data: &[T]) -> Vec<T> {
+        let mut result = Vec::new();
+        let mut candidates: Vec<T> = data.iter().map(|t| t.clone()).collect();
+        let range = Uniform::new(0, candidates.len());
+        let mut rng = thread_rng();
+        while candidates.len() < k {
+            candidates.push(data[range.sample(&mut rng)].clone());
+        }
+        result.push(Self::remove_random(&mut candidates, range));
+        while result.len() < k {
+            let squared_distances: Vec<f64> = candidates.iter()
+                .map(|datum| 1.0f64 + distance(datum, result.last().unwrap()).into())
+                .map(|dist| dist.powf(2.0))
+                .collect();
+            let dist = WeightedIndex::new(&squared_distances).unwrap();
+            result.push(Self::remove_random(&mut candidates, dist));
+        }
+        result
     }
-    result.push(remove_random(&mut candidates, range));
-    while result.len() < k {
-        let squared_distances: Vec<f64> = candidates.iter()
-            .map(|datum| 1.0f64 + distance(datum, result.last().unwrap()).into())
-            .map(|dist| dist.powf(2.0))
+
+    pub fn remove_random<P: Distribution<usize>>(candidates: &mut Vec<T>, distribution: P) -> T {
+        let mut rng = thread_rng();
+        let end = candidates.len() - 1;
+        let choice = distribution.sample(&mut rng);
+        candidates.swap(end, choice);
+        candidates.remove(end)
+    }
+
+    fn kmeans_iterate<M: Fn(&Vec<&T>) -> T>(k: usize, data: &[T], distance: &D, mean: &M) -> Vec<T> {
+        let mut result = Self::initial_plus_plus(k, distance, data);
+        loop {
+            let mut classifications: Vec<Vec<&T>> = (0..k).map(|_| Vec::new()).collect();
+            for datum in data {
+                let category = Self::classify(datum, &result, distance);
+                classifications[category].push(datum);
+            }
+            let prev = result;
+            result = classifications.iter().enumerate()
+                .map(|(i, c)|
+                    if c.is_empty() {
+                        prev[i].clone()
+                    } else {
+                        mean(c)
+                    }
+                ).collect();
+
+            if result.iter().enumerate().all(|(i,r)| &prev[i] == r) {
+                return result;
+            }
+        }
+    }
+
+    fn classify(target: &T, means: &Vec<T>, distance: &D) -> usize {
+        let distances: Vec<(V,usize)> = (0..means.len())
+            .map(|i| (V::from(distance(&target, &means[i])), i))
             .collect();
-        let dist = WeightedIndex::new(&squared_distances).unwrap();
-        result.push(remove_random(&mut candidates, dist));
+        distances.iter()
+            .fold(None, |m:Option<&(V, usize)>, d| m.map_or(Some(d), |m|
+                Some(if m.0 < d.0 {m} else {d}))).unwrap().1
     }
-    result
-}
-
-pub fn remove_random<T: Clone, D: Distribution<usize>>(candidates: &mut Vec<T>, distribution: D) -> T {
-    let mut rng = thread_rng();
-    let end = candidates.len() - 1;
-    let choice = distribution.sample(&mut rng);
-    candidates.swap(end, choice);
-    candidates.remove(end)
-}
-
-fn kmeans_iterate<T: Clone + PartialEq, V: Copy + PartialEq + PartialOrd + Into<f64>, D: Fn(&T,&T) -> V, M: Fn(&Vec<&T>) -> T>
-(k: usize, data: &[T], distance: &D, mean: &M) -> Vec<T> {
-    let mut result = initial_plus_plus(k, distance, data);
-    loop {
-        let mut classifications: Vec<Vec<&T>> = (0..k).map(|_| Vec::new()).collect();
-        for datum in data {
-            let category = classify(datum, &result, distance);
-            classifications[category].push(datum);
-        }
-        let prev = result;
-        result = classifications.iter().enumerate()
-            .map(|(i, c)|
-                if c.is_empty() {
-                    prev[i].clone()
-                } else {
-                    mean(c)
-                }
-            ).collect();
-
-        if result.iter().enumerate().all(|(i,r)| &prev[i] == r) {
-            return result;
-        }
-    }
-}
-
-fn classify<T: Clone + PartialEq, V: Copy + PartialEq + PartialOrd, D: Fn(&T,&T) -> V>(target: &T, means: &Vec<T>, distance: &D) -> usize {
-    let distances: Vec<(V,usize)> = (0..means.len())
-        .map(|i| (distance(&target, &means[i]).into(), i))
-        .collect();
-    distances.iter()
-        .fold(None, |m:Option<&(V, usize)>, d| m.map_or(Some(d), |m|
-            Some(if m.0 < d.0 {m} else {d}))).unwrap().1
 }
 
 #[cfg(test)]
